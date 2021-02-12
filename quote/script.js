6a64bba3947f9cb0e515e5fd0d19d6921ebb748c
kweku76 <daffran@gmail.com>
2021-02-12 22:01:01 +1000

    style

--- quote/script.js
+++ quote/script.js
@@ -3,10 +3,24 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 //each of the contsants above will target the html elements in our index page
 let apiQuotes = []; //we set the apiquotes variable as an empty array. we are using let because the value of apiQuotes changes.
 // show new quote
+
+// show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+// hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 function newQuote() {
+  loading();
   // pick a random quote from api
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   console.log(quote);
@@ -28,12 +42,15 @@ function newQuote() {
     //..otherwise...
     quoteText.classList.remove("long-quote"); //..dont use long-text class
   }
+  //set quote, hide loader
   quoteText.textContent = quote.text;
   //this sets the value of text content to be shown in the html element.
+  complete();
 }
 
 // Get quote fROM API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
